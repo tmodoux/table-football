25acdcb10e56a744ceff6602f4cce084f46168a2
Thieb Modoux <tmodoux>
2023-08-01 11:14:48 +0200

    add createPlayer endpoint

--- backend/src/resolvers/PlayerResolver.ts
+++ backend/src/resolvers/PlayerResolver.ts
@@ -1,10 +1,24 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
 import { Player } from "../models/Player";
+import { CreatePlayerInput } from "../inputs/CreatePlayerInput";
 
 @Resolver()
 export class PlayerResolver {
   @Query(() => [Player])
   players() {
     return Player.find();
+  }
+
+  @Mutation(() => Player)
+  async createPlayer(@Arg("data") data: CreatePlayerInput) {
+    const player = Player.create({
+      name: data.name,
+      wins: 0,
+      losses: 0,
+      goalsAgainst: 0,
+      goalsFor: 0,
+    });
+    await player.save();
+    return player;
   }
 }
