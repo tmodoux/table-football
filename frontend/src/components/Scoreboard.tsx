bb0f59f57f1e21d6178444db026e2a382e297397
tmodoux <thiebaud.modoux@gmail.com>
2023-08-03 17:38:04 +0200

    sort scoreboard

--- frontend/src/components/Scoreboard.tsx
+++ frontend/src/components/Scoreboard.tsx
@@ -1,5 +1,5 @@
 import { PlayerType } from "../App";
-import { Table } from "antd";
+import { Alert, Table } from "antd";
 
 type ScoreboardPropsType = {
   players?: PlayerType[];
@@ -48,16 +48,28 @@ const Scoreboard = ({ players = [] }: ScoreboardPropsType) => {
       gamesPlayed: player.played,
       wins: player.wins,
       losses: player.losses,
-      ratio: player.wins ? player.played / player.wins : "-",
+      ratio: player.played ? +(player.wins / player.played).toPrecision(2) : 0,
       goalsFor: player.goalsFor,
       goalsAgainst: player.goalsAgainst,
       goalsDifference: player.goalsFor - player.goalsAgainst,
     };
+  }).sort((p1, p2) => {
+    if (p1.ratio !== p2.ratio) {
+      return p2.ratio - p1.ratio;
+    }
+    if (p1.goalsDifference !== p2.goalsDifference) {
+      return p2.goalsDifference - p1.goalsDifference;
+    }
+    if (p1.goalsFor !== p2.goalsFor) {
+      return p2.goalsFor - p1.goalsFor;
+    }
+    return p1.playerName.localeCompare(p2.playerName);
   });
 
   return (
     <div>
-      <h2>Scoreboard</h2>
+      <h2><u>Scoreboard</u></h2>
+      <Alert banner type="info" description="Players are ranked by Ratio first, then Goals difference, then Goals For, then Player name." showIcon={false} />
       <Table columns={columns} dataSource={data} />
     </div>
   );
